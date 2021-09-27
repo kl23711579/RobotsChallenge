05365040c0e776b6ee9fc5c2bb68239c7a17ac70
kl23711579 <kl23711579@gmail.com>
2021-09-27 21:32:31 +1000

    single responsibility principle

--- table.py
+++ table.py
@@ -1,90 +1,109 @@
 class Table:
-    def __init__(self):
+    def __init__(self, actions):
         self.robots = {}
         self.current_robot = ""
+        self.actions = actions
 
     def receive_input(self, input_str):
         input_strs = input_str.lower().split(" ")
-        if input_strs[0] == "place":
-            self.place_robot(input_strs[1])
-        elif len(self.robots) > 0: # there exist robots in table
-            if input_strs[0] == "robot":
-                self.change_robot(input_strs[1])
-            elif input_strs[0] == "move":
-                self.move()
-            elif input_strs[0] == "left":
-                self.turn_left()
-            elif input_strs[0] == "right":
-                self.turn_right()
-            elif input_strs[0] == "report":
-                self.report()
-        #     else:
-        #         print("\nERROR: INVALID COMMENT!!!")
-        # else:
-        #     print("\nERROR: NO ROBOTS ON TABLE!!!")
+        try:
+            self.actions[input_strs[0]](self, input_str)
+        except Exception as e:
+            print(e)
+            print("\nERROR: INVALID COMMENT!!!")
 
-    def place_robot(self, input_str):
-        pos1, pos2, direction = input_str.split(",")
-        pos1 = int(pos1)
-        pos2 = int(pos2)
-        is_vaild, err = self.check_pos([pos1, pos2])
-        if is_vaild:
-            robot_num = str(len(self.robots)+1)
-            self.robots[robot_num] = { "position": [pos1, pos2], "direction": direction }
-            # set current robot if there do not have current robot
-            if self.current_robot == "":
-                self.current_robot = "1"
-        # else:
-        #     print("\nERROR: %s" % (err))
+    def get_robots(self):
+        return self.robots
 
-    def move(self):
+    def set_current_robot(self, new_current_robot):
+        self.current_robot = new_current_robot
+
+    def get_current_robot(self):
+        return self.current_robot
+
+
+def place_robot(table, input_str):
+    robots, current_robot = table.get_robots(), table.get_current_robot()
+    input_str = input_str
+    pos1, pos2, direction = input_str.lower().split(" ")[1].split(",")
+    pos1 = int(pos1)
+    pos2 = int(pos2)
+    is_vaild, err = check_pos(table, [pos1, pos2])
+    if is_vaild:
+        robot_num = str(len(robots)+1)
+        robots[robot_num] = { "position": [pos1, pos2], "direction": direction }
+        # set current robot if there do not have current robot
+        if current_robot == "":
+            table.set_current_robot("1")
+    # else:
+    #     print("\nERROR: %s" % (err))
+
+def move(table, command = None):
+    robots, current_robot = table.get_robots(), table.get_current_robot()
+    if len(robots) >= 1:
         move_steps = { "north": [0, 1], "south": [0, -1], "west": [-1, 0], "east": [1, 0] }
-        new_pos = [sum(x) for x in zip(self.robots[self.current_robot]["position"], move_steps[self.robots[self.current_robot]["direction"]]) ]
-        is_vaild, err = self.check_pos(new_pos)
+        new_pos = [sum(x) for x in zip(robots[current_robot]["position"], move_steps[robots[current_robot]["direction"]]) ]
+        is_vaild, err = check_pos(table, new_pos)
         if is_vaild:
-            self.robots[self.current_robot]["position"] = new_pos
+            robots[current_robot]["position"] = new_pos
         # else:
         #     print("\nERROR: %s" % (err))
 
-    def change_robot(self, new_robot):
-        if int(new_robot) <= len(self.robots):
-            self.current_robot = new_robot
-        # else:
+def change_robot(table, new_robot):
+    robots = table.get_robots()
+    if int(new_robot) <= len(robots):
+        t.set_current_robot(new_robot)
+    # else:
         #     print("\nERROR: Robot not exist!!!")
 
-    def turn_left(self):
-        new_direction = { "north": "west", "west": "south", "south": "east", "east": "north" }
-        self.robots[self.current_robot]["direction"] = new_direction[self.robots[self.current_robot]["direction"]]
+def turn_left(table, command = None):
+    robots, current_robot = table.get_robots(), table.get_current_robot()
+    new_direction = { "north": "west", "west": "south", "south": "east", "east": "north" }
+    if len(robots) >= 1:
+        robots[current_robot]["direction"] = new_direction[robots[current_robot]["direction"]]
 
-    def turn_right(self):
-        new_direction = { "north": "east", "east": "south", "south": "west", "west": "north" }
-        self.robots[self.current_robot]["direction"] = new_direction[self.robots[self.current_robot]["direction"]]
+def turn_right(table, command = None):
+    robots, current_robot = table.get_robots(), table.get_current_robot()
+    new_direction = { "north": "east", "east": "south", "south": "west", "west": "north" }
+    if len(robots) >= 1:
+        robots[current_robot]["direction"] = new_direction[robots[current_robot]["direction"]]
 
-    def report(self):
-        pos1, pos2 = self.robots[self.current_robot]["position"]
-        direction = self.robots[self.current_robot]["direction"].upper()
-        robot_number = len(self.robots)
+def report(table, command = None):
+    robots, current_robot = table.get_robots(), table.get_current_robot()
+    if len(robots) >= 1:
+        pos1, pos2 = robots[current_robot]["position"]
+        direction = robots[current_robot]["direction"].upper()
+        robot_number = len(robots)
         if robot_number == 1:
             print("\nOutput: %d, %d, %s" % (pos1, pos2, direction))
         else:
-            print("\nOutput: There are %d robots in the table. Robot %s : %d, %d, %s" % (robot_number, self.current_robot, pos1, pos2, direction))
+            print("\nOutput: There are %d robots in the table. Robot %s : %d, %d, %s" % (robot_number, current_robot, pos1, pos2, direction))
 
-    def check_pos(self, pos):
-        # check is inside table
-        if pos[0] < 0 or pos[1] < 0 or pos[0] > 4 or pos[1] > 4:
-            error_msg = f"Position ({pos[0]}, {pos[1]}) is outside table."
-            return [False, error_msg]
-        else: # chech is conflict with other robot
-            for robot in self.robots.keys():
-                if pos[0] == self.robots[robot]["position"][0] and pos[1] == self.robots[robot]["position"][1]:
-                    error_msg = f"Position ({pos[0]}, {pos[1]}) exist Robot {robot}."
-                    return [False, error_msg]
+def check_pos(table, pos):
+    robots = table.get_robots()
+    # check is inside table
+    if pos[0] < 0 or pos[1] < 0 or pos[0] > 4 or pos[1] > 4:
+        error_msg = f"Position ({pos[0]}, {pos[1]}) is outside table."
+        return [False, error_msg]
+    else: # check is conflict with other robot
+        for robot in robots.keys():
+            if pos[0] == robots[robot]["position"][0] and pos[1] == robots[robot]["position"][1]:
+                error_msg = f"Position ({pos[0]}, {pos[1]}) exist Robot {robot}."
+                return [False, error_msg]
 
-        return [True, ""]
+    return [True, ""]
 
 if __name__ == '__main__':
     import sys
-    t = Table()
+    actions = {
+        "place": place_robot,
+        "robot": change_robot,
+        "move": move,
+        "left": turn_left,
+        "right": turn_right,
+        "report": report
+    }
+    t = Table(actions)
     if len(sys.argv) > 1:
         file_name = sys.argv[1]
         with open(file_name, "r") as f:
